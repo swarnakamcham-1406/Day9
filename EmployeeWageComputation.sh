fed5fca82738fc6ccede13c80dd0718c2b219c40
swarna <kamchamswarna@gmail.com>
2020-10-09 16:42:40 +0530

    Day and daily wage along with the totalWage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,50 +1,58 @@
 #! /bin/bash -x
 
 echo "Welcome to the Employee Wage Computation "
+is_part_time=1;
+is_full_time=2;
+max_hrs_in_month=100;
+emp_rate_per_hr=20;
+num_working_days=20;
 
-IS_PART_TIME=1
-IS_FULL_TIME=2
-EMP_RATE_PER_HR=20
-MAX_HRS_IN_MONTH=50
-NUM_WORKING_DAYS=20
+totalEmpHr=0;
+totalWorkingDays=0;
 
-declare -A dailyWages
+declare -A dailyWage;
 
-totalEmpHr=0
-totalWorkingDays=0
+function getWorkHrs() {
 
-function getworkinghour(){
+local $empCheck=$1
 
-local empHrs=0;
+case $empCheck in
 
-         case $1 in
-            $IS_FULL_TIME )
-                              empHrs=8
-                ;;
-            $IS_PART_TIME )
-                              empHrs=4
-                ;;
-            * )
-                           empHrs=0
-                ;;
-         esac
+$is_full_time)
+                empHrs=8
+                   ;;
+$is_part_time)
+                empHrs=4
+                  ;;
+*)
+                empHrs=0
+                 ;;
+esac
+
 echo $empHrs
 
 }
 
+function getEmpWage() {
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+local empHrs=$1
+
+echo $(($empHr*$emp_rate_per_hr))
+
+}
+while [[ $totalEmpHrs -lt $max_hrs_in_month && $totalWorkingDays -lt $num_workin_days ]]
 do
-      ((totalWorkingDays++))
-      employeeCheck=$((RANDOM%3))
 
-      empHrs=$( getworkinghour $employeeCheck )
+ ((totalWorkingDays++))
+empcheck=$((RANDOM%3));
+empHrs="$( getWorkHrs+$empHrs )"
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+day="Day"
+dailyWage["Day" $totalWorkingDays]="$( getEmpWage $empHrs )"
 
-      totalEmpHR=$((totalEmpHR+empHrs))
-      dailywages["Day_$totalWorkingDays"]=$(( $empHrs*$EMP_RATE_PER_HR ));
 done
-salary=$(($EMP_RATE_PER_HR*$totalEmpHR))
-echo "the salary is $salary";
-echo ${!dailywages[@]};
-echo ${dailywages[@]};
+
+totalSalary=$(($totalEmpHrs*$emp_rate_per_hr));
+echo ${dailyWage[@]}
+echo ${!dailyWage[@]}
 
