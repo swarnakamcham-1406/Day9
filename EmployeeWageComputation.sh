ecb6d2ab31992eeb51061017c729d2a437c6b9f5
swarna <kamchamswarna@gmail.com>
2020-10-09 12:42:18 +0530

    Check Employee Attendence

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,3 +1,18 @@
 #! /bin/bash -x
 
 echo "Welcome to the Employee Wage Computation "
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ]
+then
+
+echo "employee is present";
+
+else
+
+echo "employee is absent ";
+
+fi
+
