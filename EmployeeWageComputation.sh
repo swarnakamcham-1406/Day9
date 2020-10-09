6133e268c65ce00638316f4d4a0b0531b62a8b02
swarna <kamchamswarna@gmail.com>
2020-10-09 13:33:45 +0530

    employee wage for month

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -2,23 +2,33 @@
 
 echo "Welcome to the Employee Wage Computation "
 
-
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$(($RANDOM%3));
+numWorkingDays=20;
+
+for(( day=1; day<=numWorkingDays; day++ ))
+do
+
+   empCheck=$((RANDOM%3));
 case $empCheck in
 
    $isFullTime )
-                                                empHrs=12
-                                                        ;;
-        $isPartTime )
+                  empHrs=12
+                     ;;
+   $isPartTime )
 
-                                                empHrs=8
-                                                        ;;
-                * )
-                                                empHrs=0
+                  empHrs=8
+                     ;;
+      * )
+                  empHrs=0
 
-                                                        ;;
+                     ;;
 esac
+
+salary=$(($empHrs * $empRatePerHr));
+
+totalSalary=$(($totalSalary+$salary));
+done
 
